4cfe63d610fcad451d3d91737511e9096f3e7356
unknown <psteja02@gmail.com>
2023-04-25 16:20:58 +0530

    abstract changed

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -15,10 +15,10 @@ function Home(props) {
                         <div className="descriptionAboutFBallDiv">
                             <div>
                                 <span className="abstractTxt">
-                                It is played worldwide. It is a sport that requires teamwork, game plans and strategies. For planning and strategies we need a good statistics and analysis and for that we need reports.
+                                The team sport of football is practiced all over the world. It is a sport that calls for cooperation, preparation, and strategies. We need accurate statistics and analysis for planning and strategies. The development of a football analysis and reporting system is the goal of this project. 
                                 <br/>
-                                <br/>
-        The purpose of this project is to develop Analysis & Reporting System of Football. Various reports has been generated to analyze the club as well as the individual player’s performance like the top performers, the foul makers, club’s Win/loss Ratio  and many more. It collects the data on various attributes like country, goals scored, penalty, wins, position, league. The countries have been further divided under various regions such as Europe, South-America for Region-wise Analysis.
+                                Reports such as the top performers, foul-makers, the club's win/loss ratio have been created for analyzing the performance of the team as well as the performance of each individual player. 
+                                It gathers information on a variety of factors, including league, position, victories, wins with penalties, and country. For region-wise analysis, the countries have further been separated into groups according to continents like Europe and South America.
                                 </span> 
                             </div>
                         </div>
